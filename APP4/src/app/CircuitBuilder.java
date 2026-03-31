e2393d893df69149259c1b105a4bff31741435d2
Eric Burton-Labrecque <2501671@cegeplimoilou.ca>
2026-03-31 12:05:07 -0400

    Implémentation de lireComposant

--- APP4/src/app/CircuitBuilder.java
+++ APP4/src/app/CircuitBuilder.java
@@ -1,6 +1,12 @@
 package app;
+import electronique.*;
 
-import electronique.Composant;
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 
 public class CircuitBuilder {
 
@@ -11,6 +17,29 @@ public class CircuitBuilder {
     public Composant construireCircuit(String composant){
 
         return null;
+    }
+
+    private Composant lireComposant(JsonNode node){
+        String type = node.get("type").asText();
+
+        if("resistance".equals(type)){
+            return new Resistance(node.get("valeur").asDouble());
+        } else if("parallele".equals(type)){
+            List<Composant> composantes = new ArrayList<>();
+            for(JsonNode composantNode : node.get("composants")) {
+                composantes.add(lireComposant(composantNode));
+            }
+            return new CircuitParallele(composantes);
+        } else if("serie".equals(type)){
+            List<Composant> composantes = new ArrayList<>();
+            for(JsonNode composantNode : node.get("composants")) {
+                composantes.add(lireComposant(composantNode));
+            }
+            return new CircuitSerie(composantes);
+
+        }
+        throw new IllegalArgumentException("Type de composantes inconnu : " + type);
+
     }
 
 
